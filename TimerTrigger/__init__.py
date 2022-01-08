43f0cf0109dbfb8143ea64b0aba248018596e1a7
SPWareing <sw1662@my.bristol.ac.uk>
2022-01-08 15:18:14 +0000

    Updated init.py

--- TimerTrigger/__init__.py
+++ TimerTrigger/__init__.py
@@ -3,6 +3,7 @@ import logging
 import requests
 import azure.functions as func
 import pandas as pd 
+import bs4 
 import re 
 from datetime import datetime as dt
 from dateutil.relativedelta import relativedelta
@@ -11,17 +12,34 @@ def main(mytimer: func.TimerRequest, outputblob: func.Out[bytes]):
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
     
-    #regex pattern to extract the dataset
-    def regx(string):
+    #regex pattern to extract the Historic England dataset
+    def regx_eng(string):
         pattern = r'\D+\((\d)*\d\.\d{2} MB\)'
         compd = re.compile(pattern)
         mog = compd.search(string)
         return mog.group(0)
+    #regex pattern for the Historic Scotland dataset
+    def regx_scot(string):
+        pattern = r'(?<=Dataset: )\D+'
+        compd = re.compile(pattern)
+        mog = compd.search(string)
+        return mog.group(0)
+
+    #Time delta 
+    delta = relativedelta(weeks=2)
     
     # url to the dataset page
     uri = r"https://historicengland.org.uk/listing/the-list/data-downloads/"
 
     req = requests.get(uri)
+    # urls to the Historic Scotland datasets
+    uris = [r"https://data.gov.uk/dataset/722b93f3-75fd-47ce-9f06-0efcfa010ecf/listed-buildings", 
+    r'https://data.gov.uk/dataset/9075113f-d8e3-48da-bbfc-34f58939529b/scheduled-monuments',
+    r'https://data.gov.uk/dataset/433101a0-3bd3-4035-b028-ea8d7a11abfd/conservation-areas',
+    r'https://data.gov.uk/dataset/eab6ee72-23e8-46df-b74b-c2a9cb3ee6e0/world-heritage-sites',
+    r'https://data.gov.uk/dataset/bab10bd8-cc8b-4b4e-9eb7-d620a3ee27d9/gardens-and-designed-landscapes',
+    r'https://data.gov.uk/dataset/e290e0b9-b85e-4c9a-a1a3-475acacf5dfe/battlefields-inventory-boundaries',
+    r'https://data.gov.uk/dataset/484afc0c-2b62-4218-a464-ef32a1a60a69/historic-marine-protected-areas']
 
     
     if mytimer.past_due:
@@ -30,36 +48,105 @@ def main(mytimer: func.TimerRequest, outputblob: func.Out[bytes]):
     try:
         req.raise_for_status()
         #Get the first table in the webpage
+        #create a BeautfulSoup Object
+        soup = bs4.BeautifulSoup(req.text, 'html.parser')
+        table = soup.find('table',{'class': 'download-table'})
+        #Get the modal links for the shapefiles
+        links = []
+        for tr in table.findAll("tr"):
+            trs = tr.findAll("td", {'class':"download-table__cell download-table__cell--modal-link"})
+            for each in trs:
+                try:
+                    link = each.find('a')['href']
+                    links.append(uri+link)
+                except:
+                    pass
         df = pd.read_html(req.text)[0]
+        df['Link'] = links
         #Strip the unecessary data
-        df['Dataset (.zip)'] = df.apply(lambda x: regx(x['Dataset (.zip)']), axis=1)
+        df['Dataset (.zip)'] = df.apply(lambda x: regx_eng(x['Dataset (.zip)']), axis=1)
         #change the Last updated column to datetime -- then sort
         df['Last updated'] = df['Last updated'].astype('datetime64')        
         df = df.sort_values('Last updated')
 
         df.reset_index(inplace=True, drop=True)
         
-        #create a time delta of 1 month
-        delta = relativedelta(months=1)
+        #create a time delta of 1 2 weeks        
         check = dt.now() - delta
 
         # add the column Updated to the dataframe
         df['Updated'] = df.apply(lambda x: x['Last updated']>check, axis=1)
+        df['Organisation'] = 'Historic England'
+        df.rename(columns={'Dataset (.zip)': 'Dataset'}, inplace=True)
+        df = df.iloc[:, [6,0,2,4,5]]
+
+        logging.info('The length of the HE dataset is {}'.format(len(df)))
         
-        logging.info(df.head())
         
-        #get only the updated shapefiles
-        df_updated = df[df.Updated==True]
-        logging.info(df_updated.head())
-        # save the csv string to a variable
-        output = df_updated.to_csv()
-        logging.info(output)
-        logging.info("\nUploading to Azure Storage as blob")
-        outputblob.set(output)
+        
+        
     except Exception as exc:
         logging.info('There was a problem: {}'.format(exc))
     
     
+    heritage = pd.DataFrame()
 
+    for uri in uris:
+        req = requests.get(uri)
+        try:
+            req.raise_for_status()
+            dfs = pd.read_html(req.text)[0]
+            soup = bs4.BeautifulSoup(req.text, 'html.parser')
+            table = soup.find('table')
+
+            links = []
+            for tr in table.findAll("tr"):
+                    trs = tr.findAll("td")
+                    for each in trs:
+                        try:
+                            link = each.find('a')['href']
+                            links.append(link)
+                        except:
+                            pass
+
+            dfs['Link'] = links
+            heritage = heritage.append(dfs)              
+        except Exception as e:
+            logging.info(f"There was a problem with the Historic Scotland Dataset{e}")   
+
+    if len(heritage)>0:
+        heritage = heritage[heritage['Format']=='ZIP']
+        heritage['Last updated'] = heritage['File added'].astype('datetime64')
+        heritage.drop(columns=['File added'], inplace=True)
+        heritage['Dataset'] = heritage.apply(lambda x: regx_scot(x['Link to the data']), axis=1)
+        last_column = heritage.pop('Dataset')
+        heritage.insert(0, 'Dataset', last_column)
+        delta = relativedelta(months=1)
+        recall = dt.now()-delta
+
+        heritage['Updated'] = heritage.apply(lambda x: x['Last updated']> recall, axis=1)
+        heritage.reset_index(inplace=True, drop=True)
+        heritage['Organisation'] = 'Historic Scotland'
+        heritage = heritage.iloc[:,[7,0,5,4,6]]
+        combined = df.append(heritage)
+    
+    else:
+        logging.info('The Heritage dataset was empty')
+        combined = df
+
+    #get only the updated shapefiles
+    df_updated = combined[combined['Updated']==True]
+    logging.info('The size of the Updates dataset is {}'.format(len(df_updated)))
+    if len(df_updated)>0:
+        # save the csv string to a variable
+        output = df_updated.to_csv()
+    else:
+        output = 'No Datasets have been updated in the last two weeks'
+    logging.info(df_updated.head())
+    
+    
+    logging.info(output)
+    logging.info("\nUploading to Azure Storage as blob")
+    outputblob.set(output)    
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
